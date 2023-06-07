beba9cf904d21cc9bf45b76a4ca0e05715000739
mk3346 <mk3346@hotmail.de>
2023-06-07 16:17:13 +0200

    session handling introduced

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcryptjs = require("bcryptjs");
+const mongoose = require('mongoose');
 
 const User = require('../models/User.model');
 
@@ -20,6 +21,22 @@ router.post("/signup", (req, res, next) => {
 
     const {email, password} = req.body;
 
+
+    // make sure users fill all mandatory fields:
+    if (!email || !password) {
+        res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your email and password.' });
+        return; // finish execution of the current function
+    }
+
+
+    // make sure passwords are strong:
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+        res.status(400).render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+        return;
+    }
+
+
     bcryptjs
         .genSalt(saltRounds)
         .then(salt => {
@@ -35,17 +52,68 @@ router.post("/signup", (req, res, next) => {
             return User.create(newUser);
         })
         .then( userFromDB => {
-            res.redirect("/user-profile");
+            res.redirect("/user-profile"); // user has been created, redirect to user profile
         })
         .catch(error => {
             console.log("error creating account...", error);
-            next(error)
+
+            if (error instanceof mongoose.Error.ValidationError) {
+                console.log("this is a mongoose validator error")
+                res.status(400).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000) {
+                res.status(400).render('auth/signup', { errorMessage: 'Email needs to be unique.' });
+            } else {
+                console.log("this is NOT a mongoose validator error")
+                next(error)
+            }
+
         });
 });
 
 
+//GET /login
+router.get("/login", (req, res, next) => {
+    res.render("auth/login");
+});
+
+
+
+//POST /login
+router.post("/login", (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (email === '' || password === '') {
+        res.status(400).render('auth/login', { errorMessage: 'Please enter both, email and password to login.' });
+        return;
+    }
+
+    User.findOne({email: email})
+        .then( user => {
+            if (!user) {
+                //user doesn't exist (mongoose returns "null")
+                res.status(400).render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+                return;
+            } else if (bcryptjs.compareSync(password, user.passwordHash)){
+                //login successful
+                res.render("auth/user-profile", {userDetails: user});
+            } else {
+                //login failed
+                res.status(400).render('auth/login', { errorMessage: 'Incorrect credentials.' });
+            }
+        })
+        .catch(error => {
+            console.log("error trying to login...", error);
+            next(error);
+        });
+
+});
+
+
+
 //GET user-profile
-router.get('/user-profile', (req, res) => res.send('this is your user profile'));
+router.get('/user-profile', (req, res) => {
+    res.render("auth/user-profile");
+});
 
 
 module.exports = router;
